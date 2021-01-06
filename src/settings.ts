002ea59739b85b1b8d9af4cd78411617da523a77
Matthew Hillman <mhillman@bomgar.com>
2021-01-05 22:31:34 -0600

    Rename to try to avoid spam detection

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'TotalConnectSecurityTwoDotZero';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-tc-2-0';
+export const PLUGIN_NAME = 'homebridge-tc-security-panel';
